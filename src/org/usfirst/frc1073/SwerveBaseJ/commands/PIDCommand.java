b47ff61a5eb8d355dbd0febb3c576a85eafe7212
Derek Wider <technoD@outlook.com>
2015-11-11 19:20:12 -0500

    Added the PID interfaces

---
+++ src/org/usfirst/frc1073/SwerveBaseJ/commands/PIDCommand.java
@@ -0,0 +1,11 @@
+package org.usfirst.frc1073.SwerveBaseJ.commands;
+/**
+ * 
+ * @author Derek Wider
+ *	An interface to be implemented by any command that is to interact with a PIDThread.
+ */
+public interface PIDCommand {
+	public double getPIDSetpoint(int marker);
+	public boolean isPIDEnabled();
+	
+}
